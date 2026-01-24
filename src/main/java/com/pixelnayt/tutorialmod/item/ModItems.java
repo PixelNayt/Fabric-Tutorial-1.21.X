7f66e5073f8b196ec5ac5b27b8a3c1448adf3f5f
PixelNayt <85763076+PixelNayt@users.noreply.github.com>
2026-01-25 02:55:35 +0800

    Added Pink Garnet and Raw Pink Garnet Item to Minecraft

---
+++ src/main/java/com/pixelnayt/tutorialmod/item/ModItems.java
@@ -0,0 +1,32 @@
+package com.pixelnayt.tutorialmod.item;
+
+import com.pixelnayt.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    //Create Item Instance
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    //Helper Method for registering items
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        //Add to Creative Mode Tab
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
